8ae9beeaa0b153fc6d44befc55ac88ab4ddda57e
lagonzalezd <la.gonzalezd33@gmail.com>
2020-04-25 22:40:28 -0500

    Mas test

--- test/data_structures/ArbolRojoNegroTest.java
+++ test/data_structures/ArbolRojoNegroTest.java
@@ -1,6 +1,7 @@
 package data_structures;
 
 import model.data_structures.ArbolRojoNegro;
+import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,7 +18,7 @@ public class ArbolRojoNegroTest {
         }
     }
 
-
+    @Test
     public void testsize() {
         setUp1();
 
@@ -25,52 +26,66 @@ public class ArbolRojoNegroTest {
 
     }
 
+    @Test
     public void testisEmpty() {
         assertTrue(arbol.isEmpty());
 
     }
 
+    @Test
     public void testGet() {
 
     }
 
+    @Test
     public void testContains() {
-        arbol.contains()
     }
 
+    @Test
     public void testPut() {
     }
 
+    @Test
     public void testDeleteMin() {
     }
 
+    @Test
     public void testDeleteMax() {
     }
 
+    @Test
     public void testDelete() {
     }
 
+    @Test
     public void testHeight() {
     }
 
+    @Test
     public void testMin() {
     }
 
+    @Test
     public void testMax() {
     }
 
+    @Test
     public void testFloor() {
     }
 
+    @Test
     public void testCeiling() {
     }
 
+    @Test
     public void testSelect() {
     }
 
+    @Test
     public void testRank() {
     }
 
+    @Test
     public void testKeys() {
     }
 
---
+++ test/data_structures/MaxColaTest.java
@@ -0,0 +1,38 @@
+package data_structures;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+class MaxColaTest {
+
+    @Before
+    public void setUp1() {
+
+
+    }
+
+    @Test
+    public void isEmpty() {
+
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void max() {
+    }
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void delMax() {
+    }
+
+    @Test
+    public void iterator() {
+    }
+}
